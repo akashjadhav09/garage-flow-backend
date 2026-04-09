73b056a3b30ff564f0af402d870011247f1414b4
Akash Jadhav <akash.j2991@gmail.com>
2026-04-09 23:08:32 +0530

    Initial setup.

---
+++ garage-flow-be/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+export default app;
